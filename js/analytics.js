f4058dc4bfd0b643ef0d9045951bb491640879d3
boboyu <boboyu801103@outlook.com>
2024-12-12 04:27:45 +0800

    更新 Google Analytics 初始化代碼

--- js/analytics.js
+++ js/analytics.js
@@ -1,6 +1,14 @@
 // Google Analytics
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
+
+// 初始化 Google Analytics
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','G-V69YEQ8EHQ');
+
 gtag('js', new Date());
 gtag('config', 'G-V69YEQ8EHQ');
 
@@ -15,12 +23,18 @@ function trackEvent(action, category, label) {
 // 追蹤按鈕點擊
 document.addEventListener('DOMContentLoaded', function() {
     // 追蹤開始新局
-    document.getElementById('start-game').addEventListener('click', function() {
-        trackEvent('click', 'game', 'start_new_game');
-    });
+    const startGameBtn = document.getElementById('start-game');
+    if (startGameBtn) {
+        startGameBtn.addEventListener('click', function() {
+            trackEvent('click', 'game', 'start_new_game');
+        });
+    }
 
     // 追蹤幫我選號
-    document.getElementById('random-pick').addEventListener('click', function() {
-        trackEvent('click', 'game', 'random_pick');
-    });
+    const randomPickBtn = document.getElementById('random-pick');
+    if (randomPickBtn) {
+        randomPickBtn.addEventListener('click', function() {
+            trackEvent('click', 'game', 'random_pick');
+        });
+    }
 });
